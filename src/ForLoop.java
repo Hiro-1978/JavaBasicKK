f58a772d82b56f73eb976776e3f59be3e6a79f49
Hiro-1978 <hiromiuki11@gmail.com>
2021-04-16 16:13:04 +0700

    java basic kk
    forloop

---
+++ src/ForLoop.java
@@ -0,0 +1,21 @@
+
+public class ForLoop {
+    public static void main(String[] args){
+    // 3 ส่วน
+    // ส่วนตัวแปรเริ่มต้น => ตัวนับรอบ
+    // เงื่อนไข
+    // การเพิ่มค่า / ลดค่า
+    
+    /*
+        for(ตัวแปรเริ่มต้น;เงื่อนไข;การเพื่มค่า / ลดค่า){
+        * คำสั่ง
+    }
+    */
+      //  int count; ใช้ได้ทั่วไป
+        for(int count=1;count<=10;count++){              // ใช้ได้เฉพาะใน forloop
+            // คำสั่งที่จะทำซ้ำ
+            System.out.println("Hello World");
+            
+        }
+    }
+}
